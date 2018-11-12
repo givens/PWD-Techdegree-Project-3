528a72b46e5d42fca86bc18c790a429487de543b
BG <bgivens@pm.me>
2018-11-11 19:50:47 -0500

    PEP8

--- worklog.py
+++ worklog.py
@@ -1,129 +1,162 @@
+"""
+worklog.py
+----------
+Module contains Worklog class.
+Worklog provides menus and program flow
+for the PWD-Techdegree-Project-3 cli tool.
+"""
+
 from datetime import datetime
 import menu
 import sys
+import os
+import pdb
 
-from tasks import Task, TaskList
-from entry import Entry
-from search import Search
-from edit import Edit
-from utils import clear, clear_screen, fmt, wait, pause_screen, write_tasks
+import tasks
+import entry
+import search
+import edit
+import utils
 
 
 class Worklog:
+    """Worklog menus and program flow"""
 
     def __init__(self):
-        self.tl = TaskList.import_tasklist()
+        """
+        Load json file
+        If no file exists, create an empty TaskList
+        """
+        if os.path.exists(utils.file_name):
+            self.tl = tasks.TaskList.import_tasklist()
+        else:
+            self.tl = tasks.TaskList()
 
     def quit_program(self):
-        write_tasks(self.tl)
+        """
+        Quit program by writing to json file
+        Print goodbye message
+        """
+        utils.write_tasks(self.tl)
         print("Goodbye!")
         sys.exit(0)
 
-    @pause_screen
-    @clear_screen
+    @utils.pause_screen
+    @utils.clear_screen
     def main_menu(self):
+        """
+        Main menu provides three options
+        1 - Add new entry
+        2 - Search existing entries
+        3 - Quit program
+        """
         while True:
             choice = menu.make(menu.main)
-            if choice == 1: # add new entry
-                task = Entry().enter_all()
-                self.tl.add(task) # or write to file
+            if choice == 1:  # add new entry
+                task = entry.Entry().enter_all()
+                self.tl.add(task)
                 self.main_menu()
-            elif choice == 2: # search existing entries
+            elif choice == 2:  # search existing entries
                 if not self.tl:
                     print("Cannot search, no entries exist.")
-                    wait()
-                self.search_menu()
-            elif choice == 3: # quit program
+                    utils.wait()
+                    self.main_menu()
+                else:
+                    self.search_menu()
+            elif choice == 3:  # quit program
                 self.quit_program()
 
-    @pause_screen
-    @clear_screen
+    @utils.pause_screen
+    @utils.clear_screen
     def search_menu(self):
-        #pdb.set_trace()
+        """
+        Search menu provides 6 options
+        1 - Search by date
+        2 - Search by date range
+        3 - Search by time
+        4 - Search by contains (this is the "exact search")
+        5 - Search by pattern
+        6 - Return to main menu
+        """
         while True:
             choice = menu.make(menu.search)
-            if choice == 1: # search by exact date
-                tasks = Search().search_by_date(self.tl)
-            elif choice == 2: # search by range of dates
-                tasks = Search().search_by_date_range(self.tl)
-            elif choice == 3: # search by time
-                tasks = Search().search_by_time(self.tl)
-            elif choice == 4: # exact string search
-                tasks = Search().search_by_contains(self.tl)
-            elif choice == 5: # pattern search
-                tasks = Search().search_by_pattern(self.tl)
+            if choice == 1:  # search by exact date
+                tasks = search.Search().search_by_date(self.tl)
+            elif choice == 2:  # search by range of dates
+                tasks = search.Search().search_by_date_range(self.tl)
+            elif choice == 3:  # search by time
+                tasks = search.Search().search_by_time(self.tl)
+            elif choice == 4:  # exact string search
+                tasks = search.Search().search_by_contains(self.tl)
+            elif choice == 5:  # pattern search
+                tasks = search.Search().search_by_pattern(self.tl)
             elif choice == 6:
                 self.main_menu()
-            else:
-                self.search_menu()
-            for task in tasks:
-                print(task)
+
             if tasks:
                 self.edit_menu(tasks)
-    #            replace_tasks(tasks, new_tasks)
             else:
-                print("No tasks found")
+                print("No tasks found.")
                 self.search_menu()
 
-    @pause_screen
-    def edit_menu(self,tasks):
-        ed = Edit(tasks)
+    @utils.pause_screen
+    def edit_menu(self, tasks):
+        """
+        edit.Edit menu provides 6 options
+        1 - Next item
+        2 - Previous item
+        3 - Edit item
+        4 - Delete item
+        5 - Return to search menu
+        6 - Return to main menu
+        """
+        # pdb.set_trace()
+        ed = edit.Edit(tasks)
         while True:
-            clear()
-            ed.print_task()
+            utils.clear()
+            try:
+                ed.print_task()
+            except IndexError as err:
+                print(err)
+                self.search_menu()
             choice = menu.make(menu.edit)
             try:
-                if choice==1: # next
+                if choice == 1:  # next
                     ed.next()
-                elif choice==2: # previous
+                elif choice == 2:  # previous
                     ed.previous()
-                elif choice==3: # edit
+                elif choice == 3:  # edit
                     old_task, new_task = ed.edit_task()
                     self.tl.edit(old_task, new_task)
-                elif choice==4: # delete
+                elif choice == 4:  # delete
                     old_task = ed.delete_task()
                     self.tl.delete(old_task)
-                elif choice==5: # search menu
-                    print("search menu")
+                elif choice == 5:  # search menu
                     self.search_menu()
-                elif choice==6: # main menu
-                    print("main menu")
+                elif choice == 6:  # main menu
                     self.main_menu()
             except IndexError as err:
                 print(err)
                 self.search_menu()
 
+
 if __name__ == '__main__':
     """
-    Creates an instance of our working class, and
-    calls the run method which will handle the main problem logic.
+    Testing and validation
     """
-    d1 = datetime.strptime("20181105", fmt)
-    d2 = datetime.strptime("20181106", fmt)
-    ts = [('clean room', 15, d1, 'pick up under bed'),
-        ('clean office', 20, d1, 'remove DMD'),
-        ('treehouse practice', 60, d2, 'python'),
-        ('treehouse project 3', 120, d2, 'work log'),
-        ('check cryptocurrency', 15, d2, 'is bitcoin up?')]
-
-    ts2 = [
-    {'title': 'clean room', 'notes': 'pick up under bed', 'minutes': 15, 'date': '20181105'},
-    {'title': 'clean office', 'notes': 'remove DMD', 'minutes': 20, 'date': '20181105'},
-    {'title': 'treehouse practice', 'notes': 'python', 'minutes': 60, 'date': '20181205'},
-    {'title': 'treehouse project 3', 'notes': 'work log', 'minutes': 120, 'date': '20181205'},
-    {'title': 'check cryptocurrency', 'notes': 'is bitcoin up?', 'minutes': 15, 'date': '20181205'}
-    ]
-    tl = TaskList.create_tasklist(ts)
-    write_tasks(tl)
-    tl = TaskList.import_tasklist()
+    d1 = datetime.strptime("20181105", utils.fmt)
+    d2 = datetime.strptime("20181106", utils.fmt)
+    d3 = datetime.strptime("20181107", utils.fmt)
+    ts = [('Organize filecase', 15, d1, 'pick up under bed'),
+          ('clean office', 20, d1, 'remove DMD'),
+          ('treehouse practice', 60, d2, 'python'),
+          ('treehouse project 1', 180, d1, 'guessing game'),
+          ('treehouse project 2', 480, d1, 'soccer league'),
+          ('treehouse project 3', 1200, d2, 'work log'),
+          ('check cryptocurrency', 15, d2, 'is bitcoin up?'),
+          ('segmentation', 1000, d3, 'segment rods and screws from CT image')]
+    tl = tasks.TaskList.create_tasklist(ts)
+    utils.write_tasks(tl)
+    tl = tasks.TaskList.import_tasklist()
     for task in tl.tasks:
         print(task)
-
-
-
-
-    #Worklog(ts).run()
-
-    # logwrite()
-    # writer.writeheader()
-
