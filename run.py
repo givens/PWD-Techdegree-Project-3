892d0ca81917404931894ee2e14b8a6d55a96f80
BG <bgivens@pm.me>
2018-11-11 19:53:42 -0500

    PEP8

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
+"""
+run.py
+----------
+Module runs worklog  for the PWD-Techdegree-Project-3 cli tool.
+"""
+
 from worklog import Worklog
 
-if __name__=='__main__':
+if __name__ == '__main__':
     Worklog().main_menu()
