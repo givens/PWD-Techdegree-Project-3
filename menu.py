5bbf067096ef30e30dc0cefc0331fa5fc8a7ace2
BG <bgivens@pm.me>
2018-11-11 21:08:12 -0500

    PEP8

--- menu.py
+++ menu.py
@@ -1,37 +1,45 @@
-from utils import print_error, clear_screen
+"""
+menu.py
+-------
+This module makes the standard menu.
+"""
+
 import time
 
+import utils
+
 main = [
-'What would you like to do?',
-'Add new entry',
-'Search existing entries',
-'Quit program',
+    'What would you like to do?',
+    'Add new entry',
+    'Search existing entries',
+    'Quit program',
 ]
 
 search = [
-'How would you like to search?',
-'Exact Date',
-'Range of Dates',
-'Time',
-'Exact Search',
-'Pattern Search',
-'Return to main menu',
+    'How would you like to search?',
+    'Exact Date',
+    'Range of Dates',
+    'Time',
+    'Exact Search',
+    'Pattern Search',
+    'Return to main menu',
 ]
 
 edit = [
-'How do you want to proceed?',
-'Next',
-'Previous',
-'Edit',
-'Delete',
-'Return to search menu',
-'Return to main menu',
+    'How do you want to proceed?',
+    'Next',
+    'Previous',
+    'Edit',
+    'Delete',
+    'Return to search menu',
+    'Return to main menu',
 ]
 
-def make(menu):
 
+def make(menu):
+    "Makes standard menu"
     for index, menu_item in enumerate(menu):
-        if index==0:
+        if index == 0:
             print(menu_item)
         else:
             print('{} - {}'.format(index, menu_item))
@@ -41,12 +49,8 @@ def make(menu):
         if choice not in range(1, len(menu)):
             raise ValueError("Menu choice not listed.")
     except ValueError as err:
-        print_error(err)
-        time.sleep(0.5)
+        utils.print_error(err)
+        utils.wait()
         return make(menu)
     else:
         return choice
-
-
-
-
