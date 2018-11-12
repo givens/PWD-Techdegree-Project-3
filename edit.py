12cc667e90f4d8f73e2dc7c5668099d5cc89e7b3
BG <bgivens@pm.me>
2018-11-11 21:01:34 -0500

    PEP8

--- edit.py
+++ edit.py
@@ -1,48 +1,52 @@
-from tasks import TaskList
-from datetime import datetime
-from utils import fmt
-from entry import Entry
-import pdb
+"""
+edit.py
+-------
+Edits module for editing and deleting tasks.
+"""
+
+import entry
+
 
 class Edit:
+    "Edit class modifies tasks and moves through them."
 
     def __init__(self, tasks):
         self.tasks = tasks
         self.index = 0
 
     def __len__(self):
+        "Return length of taksk"
         return len(self.tasks)
 
-    def print_tasks(self):
-        if not self.tasks:
-            raise IndexError("Finished.")
-        for index, task in enumerate(tasks):
-            print('{} - {}'.format(index, task))
-
     def print_task(self):
+        "Prints current task in list"
+        if self.index < 0 or self.index >= len(self):
+            raise IndexError("No more found.")
         print(self.tasks[self.index])
 
     def edit_task(self):
+        "Edits current task in list"
         old_task = self.tasks[self.index]
-        self.tasks[self.index] = Entry().enter_all()
+        self.tasks[self.index] = entry.Entry().enter_all()
         new_task = self.tasks[self.index]
         self.index = 0
         return old_task, new_task
 
-
     def delete_task(self):
+        "Deletes current task in list"
         old_task = self.tasks[self.index]
         del self.tasks[self.index]
         self.index = 0
         return old_task
 
     def next(self):
+        "Moves to next task in list"
         self.index += 1
-        if self.index>=len(self):
+        if self.index >= len(self):
             raise IndexError("No more found.")
 
     def previous(self):
-        #pdb.set_trace()
+        "Moves to previous task in list"
         self.index -= 1
-        if self.index<0:
+        if self.index < 0:
             raise IndexError("No more found.")
